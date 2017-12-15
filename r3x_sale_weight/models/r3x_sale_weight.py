ed769d198331d93db11f43594e4ba7b2fcd22f06
chaitanya dasadiya <c.dasadiya74@gmail.com>
2017-12-15 17:54:13 +0530

    [IMP] remove whitespace

--- r3x_sale_weight/models/r3x_sale_weight.py
+++ r3x_sale_weight/models/r3x_sale_weight.py
@@ -1,6 +1,8 @@
-# Part of RU3IX.See __manifest__.py file for full copyright and licensing details.
+# Part of RU3IX licensing
+# See __manifest__.py file for full copyright and licensing details.
 from odoo import models, fields, api
 from odoo.addons import decimal_precision as dp
+
 
 class sale_order(models.Model):
     """ Sale order """
@@ -15,16 +17,16 @@ class sale_order(models.Model):
                 self.weight += line.th_weight
 
     weight = fields.Float(
-    compute='_cal_weight',
-    string='Weight',
-    digits=dp.get_precision('Stock Weight'))
+        compute='_cal_weight',
+            string='Weight',
+                digits=dp.get_precision('Stock Weight'))
 
 
 class sale_order_line(models.Model):
     """ Sale order Lines"""
     _inherit = 'sale.order.line'
     th_weight = fields.Float(
-    string='Weight',
-    readonly=True,
-    states={'draft': [('readonly', False)]},
-    digits=dp.get_precision('Stock Weight'))
+        string='Weight',
+            readonly=True,
+                states={'draft': [('readonly', False)]},
+                    digits=dp.get_precision('Stock Weight'))
